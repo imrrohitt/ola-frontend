0ad85991316cd60364e7b6558fc5c3b7f234805b
Rohit Kushwaha <rohit.kushwaha@w3villa.com>
2023-10-20 16:20:09 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Location from './Location';
 // import Home from './HomePage/Home';
+import Index from './Component/home/Index'
 
 function App() {
   return (
     <>
- <Location/>
+ {/* <Location/> */}
+ <Index />
  </>
   );
 }
