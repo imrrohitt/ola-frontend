2b81181369e94840016735f05d9614ec629352df
Khushi Gupta <khushi.gupta@meshvi.com>
2023-10-25 12:45:00 +0530

    bearer token being saved in browser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './Component/Authentication/Login'
+import Signup from './Component/Authentication/Signup'
 import { BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 import VehicleListing from './Component/Vehicle/VehicleListing';
 import Location from './Location';
@@ -9,13 +10,14 @@ import Index from './Component/home/Index'
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/location" element={<Location />} />
-      <Route path="/vehiclelisting" element={<VehicleListing />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/vehiclelisting" element={<VehicleListing />} />
+      </Routes>
+    </Router>
   );
 }
 
