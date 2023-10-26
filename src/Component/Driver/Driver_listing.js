01037d728ac1ddc0958bb350096e1de493a338a7
ANJALI JAISWAL <anjali.jaiswal@w3villa.com>
2023-10-26 11:28:45 +0530

    design driver data

--- src/Component/Driver/Driver_listing.js
+++ src/Component/Driver/Driver_listing.js
@@ -1,41 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+// CSS for styling
+import './Driver_listing.css';
 
 function Driver_listing() {
-    const [drivers, setDrivers] = useState([]);
-    const { vehicle_type } = useParams();
+  const [drivers, setDrivers] = useState([]);
+  const { vehicle_type } = useParams();
 
-    useEffect(() => {
-        console.log(vehicle_type)
-      const apiUrl = `http://127.0.0.1:3002/api/v1/drivers?vehicle_type=${vehicle_type}`;
+  useEffect(() => {
+    const apiUrl = `http://127.0.0.1:3001/api/v1/drivers?vehicle_type=${vehicle_type}`;
 
-      fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          setDrivers(data.data);
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-        });
-    }, [vehicle_type]);
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setDrivers(data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [vehicle_type]);
 
-    return (
-      <div className="App">
-        <h1>{vehicle_type} Drivers</h1>
-        <ul className="vehicle-list">
+  return (
+    <div className="container">
+      <h1 className="text-center heading">{vehicle_type} Drivers</h1>
+      <table className="table table-bordered custom-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>License Number</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
           {drivers.map((driver) => (
-            <li className="vehicle-item" key={driver.id}>
-              <div className="vehicle-details">
-                <span className="vehicle-info">Name: {driver.attributes.name}</span>
-                <span className="vehicle-info">License Number: {driver.attributes.license_number}</span>
-                <span className="vehicle-info">Phone: {driver.attributes.phone}</span>
-                <span className="vehicle-info">Email: {driver.attributes.email}</span>
-              </div>
-            </li>
+            <tr key={driver.id}>
+              <td >{driver.attributes.name}</td>
+              <td>{driver.attributes.license_number}</td>
+              <td>{driver.attributes.phone}</td>
+              <td>{driver.attributes.email}</td>
+            </tr>
           ))}
-        </ul>
-      </div>
-    );
-  }
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default Driver_listing;
+
+
--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,5 @@
 const LOCATION_API_ENDPOINT = 'http://localhost:3001/api/v1/locations';
 const VEHICLE_LISTING_API_ENDPOINT = 'http://localhost:3001/api/v1/vehicles'
-const DRIVER_LISTING_API_ENDPOINT = 'http://127.0.0.1:3002/api/v1/drivers'
+const DRIVER_LISTING_API_ENDPOINT = 'http://127.0.0.1:3001/api/v1/drivers'
 
 export {LOCATION_API_ENDPOINT ,VEHICLE_LISTING_API_ENDPOINT, DRIVER_LISTING_API_ENDPOINT};
