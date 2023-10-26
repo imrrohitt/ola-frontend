415ba05c1bef9efc816120231b9fee5794dabe91
ANJALI JAISWAL <anjali.jaiswal@w3villa.com>
2023-10-26 14:13:57 +0530

    design driver data more

--- src/Component/Driver/Driver_listing.js
+++ src/Component/Driver/Driver_listing.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-// CSS for styling
 import './Driver_listing.css';
 
 function Driver_listing() {
@@ -21,32 +20,44 @@ function Driver_listing() {
   }, [vehicle_type]);
 
   return (
-    <div className="container">
+    <div className="container mb-1 p-2">
       <h1 className="text-center heading">{vehicle_type} Drivers</h1>
-      <table className="table table-bordered custom-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>License Number</th>
-            <th>Phone</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {drivers.map((driver) => (
-            <tr key={driver.id}>
-              <td >{driver.attributes.name}</td>
-              <td>{driver.attributes.license_number}</td>
-              <td>{driver.attributes.phone}</td>
-              <td>{driver.attributes.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="row">
+        {drivers.map((driver) => (
+          <div key={driver.id} className="col-md-12 ">
+            <div className="card custom-card">
+              <div class="cab-card">
+                <img src="https://gos3.ibcdn.com/hatchback_v_5_copy-1576142454.png" alt="Cab Image" className="cab-img" />
+                <div className="cab-details">
+                  <h4 className="cab-name">{driver.attributes.name}</h4>
+                  <h1 className="text-center heading mt-4">Cab Details</h1>
+
+                  <p className="cab-type">License Number: {driver.attributes.license_number}</p>
+                  <p className="cab-type">Phone: {driver.attributes.phone}</p>
+                  <p className="cab-type">Email: {driver.attributes.email}</p>
+                  {/* <p className="cab-type">Status: {driver.attributes.status}</p> */}
+                </div>
+                <div class="rating-badge">
+
+                <div class="rating">
+               <span class="rating-number">4.4</span>
+                 <span class="rating-text">/ 5</span>
+                </div>
+              <div class="ratings-count">1031 ratings</div>
+                </div>
+
+
+              </div>
+
+            </div>
+
+          </div>
+
+        ))}
+
+      </div>
     </div>
   );
 }
 
 export default Driver_listing;
-
-
