dde27757c15b474fc0b81073297e7dc7c2dd4103
ANJALI JAISWAL <anjali.jaiswal@w3villa.com>
2023-10-26 15:02:35 +0530

    Resolved conflicts in Driver_listing.js

--- src/Component/Driver/Driver_listing.js
+++ src/Component/Driver/Driver_listing.js
@@ -9,8 +9,6 @@ function Driver_listing() {
     useEffect(() => {
         console.log(vehicle_type)
       const apiUrl = `http://127.0.0.1:3001/api/v1/drivers?vehicle_type=${vehicle_type}`;
-  useEffect(() => {
-    const apiUrl = `http://127.0.0.1:3001/api/v1/drivers?vehicle_type=${vehicle_type}`;
 
     fetch(apiUrl)
       .then((response) => response.json())
